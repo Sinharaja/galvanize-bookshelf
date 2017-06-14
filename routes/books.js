0c5ee15caa1680b42da2c09ebe21e3c050bdb24f
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-06-14 14:05:52 -0700

    Remove unnecessary code

--- routes/books.js
+++ routes/books.js
@@ -10,8 +10,6 @@ const router = express.Router();
 
 // YOUR CODE HERE
 router.get("/books", (req, res) => {
-  // knex("books")
-  //   .orderBy("title")
   let book = new Books();
   book.getAll()
     .then(result => {
@@ -21,8 +19,6 @@ router.get("/books", (req, res) => {
 
 router.get("/books/:id", (req, res) => {
   let book = new Books();
-  // knex("books")
-    // .where("id", req.params.id)
     book.getById(req.params.id)
       .then(result => {
         res.status(200).json(humps.camelizeKeys(result[0]));
@@ -30,8 +26,6 @@ router.get("/books/:id", (req, res) => {
 });
 
 router.post("/books", (req, res) => {
-  // knex("books")
-  //   .insert(humps.decamelizeKeys(req.body), ["id", "title", "author", "genre", "description", "cover_url"])
   let book = new Books();
   book.create(humps.decamelizeKeys(req.body))
     .then(result => {
@@ -40,9 +34,6 @@ router.post("/books", (req, res) => {
 });
 
 router.patch('/books/:id', (req, res) => {
-  // knex('books')
-  //   .where('id', req.params.id)
-  //   .update(humps.decamelizeKeys(req.body), ["id", "title", "author", "genre", "description", "cover_url"])
   let book = new Books();
   book.update(req.params.id, humps.decamelizeKeys(req.body))
     .then(result => {
@@ -51,15 +42,6 @@ router.patch('/books/:id', (req, res) => {
 });
 
 router.delete("/books/:id", (req, res) => {
-  // knex("books")
-  // .select("title", "author", "genre", "description", "cover_url")
-  //   .where("id", req.params.id)
-  //   .then(result => {
-  //     if (result != '') {
-  //       let output = result;
-  //       knex("books")
-  //       .where("id", req.params.id)
-  //       .del()
   let book = new Books();
   book.deleteBook(req.params.id)
     .then(result => {
