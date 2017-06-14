a3eb5b9ffa18e11e253fa93d9aebabb65156e8ea
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-06-14 16:22:50 -0700

    Complete part 3 bonus

--- routes/users.js
+++ routes/users.js
@@ -10,15 +10,30 @@ const router = express.Router();
 
 // YOUR CODE HERE
 router.post("/users", (req, res) => {
+  if (!req.body.email) {
+    return res.status(400).set('Content-Type', 'text/plain').send("Email must not be blank");
+  }
+  if (!req.body.password || req.body.password.length <= 8) {
+    return res.status(400).set('Content-Type', 'text/plain').send("Password must be at least 8 characters long");
+  }
+
   let user = new Users();
-  user.create(req.body)
+
+  user.getbyEmail(req.body.email)
     .then(result => {
-      res.status(200).json(humps.camelizeKeys(result[0]))
+      if (result.length === 0) {
+        user.create(req.body)
+        .then(result => {
+          res.status(200).json(humps.camelizeKeys(result[0]))
+        })
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(500);
+        })
+      }
+      return res.status(400).set('Content-Type', 'text/plain').send("Email already exists");
     })
-    .catch(err => {
-      console.log(err);
-      res.sendStatus(500);
-    })
+
 })
 
 module.exports = router;
--- src/model/Users.js
+++ src/model/Users.js
@@ -2,10 +2,15 @@ const bcrypt = require('bcrypt');
 const knex = require('../../knex');
 
 class Users {
+
+  getbyEmail(email) {
+    return knex("users")
+      .where("email", email)
+  }
+
   create(data) {
     return bcrypt.hash(data.password, 5)
       .then(hashedPassword => {
-        // console.log(hashedPassword);
         return knex("users")
           .insert({
             first_name: data.firstName,
