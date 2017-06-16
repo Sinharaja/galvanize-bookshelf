f50ad7bd7f09416f5f727f1d9e65464fad20a472
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-06-15 17:25:41 -0700

    Complete part 4 /users bonus

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const express = require('express');
 const knex = require('../knex');
 const humps = require('humps');
 const Users = require('../src/model/Users');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
@@ -14,18 +16,29 @@ router.post("/users", (req, res) => {
     return res.status(400).set('Content-Type', 'text/plain').send("Email must not be blank");
   }
 
-  if (!req.body.password || req.body.password.length <= 8) {
+  if (!req.body.password || req.body.password.length < 8) {
     return res.status(400).set('Content-Type', 'text/plain').send("Password must be at least 8 characters long");
   }
 
   let user = new Users();
   user.getbyEmail(req.body.email)
     .then(result => {
-      if (result.length < 0) {
+
+      if (result.length > 0) {
         return res.status(400).set('Content-Type', 'text/plain').send("Email already exists");
-      } 
+      }
+
       user.create(req.body)
       .then(result => {
+        const payload = {
+          id: result[0].id,
+          firstName: result[0].first_name,
+          lastName: result[0].last_name,
+          email: result[0].email
+        }
+        const secret = process.env.JWT_KEY;
+        const token = jwt.sign(payload, secret);
+        res.cookie("token", token, {httpOnly: true});
         res.status(200).json(humps.camelizeKeys(result[0]))
       })
       .catch(err => {
